02fd1373a194157d81b236a5e25171fae9177f5e
git-tim1 <tim.ostmann@gmx.de>
2022-09-08 17:32:04 +0200

    contact need to fill out all input fields

--- src/components/input_field.js
+++ src/components/input_field.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const InputField = ({datapoint, type, }) => {
+const InputField = ({datapoint, type, inputData }) => {
   return (
     <div>
         <label className='font-semibold text-lg mt-3 inline-block'>{datapoint}</label>
-        <input type={type} className='w-full mt-1 h-10 border border-gray-10 bg-white px-3 focus:outline-none focus:border-accent-blue-2' />
+        <input type={type} onChange={inputDataValue => inputData(inputDataValue.target.value)} className='w-full mt-1 h-10 border border-gray-10 bg-white px-3 focus:outline-none focus:border-accent-blue-2' required />
     </div>
   )
 }
