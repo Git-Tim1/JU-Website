eb6afe82696a14fc38d45a9fac6c49f26db47bd7
Git-Tim1 <tim.ostmann@gmx.de>
2023-01-22 17:17:15 +0100

    small perfectionist beauty improvements

--- src/components/event.js
+++ src/components/event.js
@@ -8,8 +8,8 @@ const Event = ({Day, Month, EventName, Description}) => {
     <div  className='w-[calc(100%-2px)] ml-[1px] cursor-pointer bg-white drop-shadow hover:drop-shadow-md'>
         <div className='w-full mx-autp h-32  rounded-sm mt-4' >
             <div className='w-[100px] 3xs:w-[105px] xs:w-[120px] sm:w-[140px] h-full float-left'>
-                <h1 className='text-[32px] leading-8 xxs:text-3xl xs:text-4xl font-extrabold text-accent-blue-2 w-full text-center inline-block mt-[36px] xxs:mt-[32px] xs:mt-[27px] ml-2 sm:ml-1'>{Day}</h1>
-                <h2 className='text-base xs:text-lg font-medium text-center w-full inline-block ml-1 sm:ml-0'>{Month}</h2>
+                <h1 className='text-[32px] leading-8 xxs:text-3xl xs:text-4xl font-extrabold text-accent-blue-2 w-full text-center inline-block mt-[36px] xxs:mt-[32px] xs:mt-[27px] '>{Day}</h1>
+                <h2 className='text-base xs:text-lg font-medium text-center w-full inline-block'>{Month}</h2>
             </div>
             <div className='float-left w-[calc(100%-100px)]  3xs:w-[calc(100%-105px)] xs:w-[calc(100%-120px)] sm:w-[calc(100%-140px)] h-full flex items-center justify-between'>
                 <h1 className='text-[15px] 4xs:text-base 3xs:text-lg xxs:text-[19px] font-bold ml-2 max-w-[80%]'>{EventName}</h1>
