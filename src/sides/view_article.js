d307845ab86ac107d7fec8c2875c7b6ffe5394a1
Git-Tim1 <tim.ostmann@gmx.de>
2023-04-09 00:41:40 +0200

    added animations

--- src/sides/view_article.js
+++ src/sides/view_article.js
@@ -4,10 +4,24 @@ import Header from '../components/header'
 import * as ReactBootStrap from 'react-bootstrap'
 import Footer from '../components/footer'
 
+const LoadingText = ({x, y, z}) => {
+  return (
+    <div className='h-4 xs:h-5 w-full mt-2 animation rounded'>
+      <div className={`background-masker btn-divide-left ${x}`}>
+      </div>
+      <div className={`background-masker btn-divide-left ${y}`}>
+      </div>
+      <div className={`background-masker btn-divide-left ${z}`}>
+      </div>
+    </div>
+  )
+}
+
 
 const ViewArticle = () => {
   const [articleData, setArticleData] = useState([])
   const [date, setDate] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   let { articleID } = useParams()
 
@@ -24,7 +38,7 @@ const ViewArticle = () => {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
         }
-      }).then((response) => response.json().then((data) => {setArticleData(data); console.log(data)}))
+      }).then((response) => response.json().then((data) => {setArticleData(data); setLoading(false)}))
     } catch (error){console.log(error)}
   }, []);
 
@@ -54,14 +68,31 @@ const ViewArticle = () => {
       <Header />
       <div className='min-h-[calc(100vh-256px)] 3xs:min-h-[calc(100vh-214px)] sm:min-h-[calc(100vh-166px)] max-w-[60rem] w-[90%] mx-auto z-1 mt-[4.5rem] pb-4 block'>
         <div className='mt-[1.5rem] block w-full h-auto float-left'>
-          <img className='aspect-video lg:max-h-[400px] lg:aspect-auto object-cover w-full' src={`https://api.wrire.com${articleData.thumbnail}`} />
-          <div className='mt-3 mb-1 bg-accent-blue-1 w-auto h-auto inline-block px-2 py-[2px]'>
+          
+          
+          
+          {!loading ? <><img className='aspect-video lg:max-h-[400px] lg:aspect-auto object-cover w-full' src={`https://api.wrire.com${articleData.thumbnail}`} /> <div className='mt-3 mb-1 bg-accent-blue-1 w-auto h-auto inline-block px-2 py-[2px]'>
             <h2 className='text-sm xxs:text-base sm:text-lg font-bold text-white'>{date} </h2>
           </div>
-          
           <h1 className='text-xl xxs:text-2xl sm:text-3xl font-extrabold'>{articleData.title}</h1>
-
-          <p className='text-sm xxs:text-base sm:text-lg mt-2 mb-10 whitespace-pre-line'>{ articleData.content }</p>
+          <p className='text-sm xxs:text-base sm:text-lg mt-2 mb-10 whitespace-pre-line'>{ articleData.content }</p></> : 
+          <div>
+            <div className='aspect-video lg:h-[400px] lg:aspect-auto object-cover w-full bg-accent-blue-2 opacity-5'></div>
+            <div className='animation-blue h-8 w-40 mt-3'>
+            </div>
+            <div className='animation h-8 md:h-10 w-3/5 mt-3 rounded'>
+            </div>
+            <div className='mt-5 mb-10'>
+              <LoadingText x="left-[15%]" y="left-[50%]" z="left-[90%]" />
+              <LoadingText x="left-[20%]" y="left-[40%]" z="left-[80%]" />
+              <LoadingText x="left-[30%]" y="left-[45%]" z="left-[60%]" />
+              <LoadingText x="left-[8%]" y="left-[25%]" z="left-[75%]" />
+              <LoadingText x="left-[22%]" y="left-[66%]" z="left-[88%]" />
+              <LoadingText x="left-[11%]" y="left-[55%]" z="left-[78%]" />
+              <LoadingText x="left-[17%]" y="left-[47%]" z="left-[85%]" />
+            </div>
+          </div>
+          }
         </div>
       </div>
       <Footer />
